f6dfacbb2c1f586423128a8651d7df01a3b62c13
root <vi-dragonfly@list.ru>
2023-06-02 20:46:41 +0000

    added files

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,14 @@ source venv/bin/activate
 git pull
 
 pip install -r requirements.txt
-npm ci --dev
-./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --publ>
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
-systemctl daemon-reload
 systemctl reload django.service
 systemctl reload nginx.service
 
 USERNAME=$(whoami)
 REVISION='1.0'
-DJANGO_ROLLBAR_TOKEN='7d6ea264d7eb401286964807e74089c7'
+echo "environment:" "$ROLLBAR_ENVIRONMENT"
 ROLLBAR_ENVIRONMENT='production'
 
 
